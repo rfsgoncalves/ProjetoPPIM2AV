fdc4bb27cf6856d32946edad2f919522de7f7c9a
RenatoFernandoSilvaGoncalves <renatogoncalves@unoeste.edu.br>
2023-10-31 13:38:53 -0300

    fix index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function processarCadastroUsuario(requisicao, resposta) {
 const app = express();
 
 //indicando para a aplicação como servir arquivos estáticos localizados na pasta 'paginas'
-app.use(express.static('./paginas')));
+app.use(express.static('./paginas'));
 
 app.get('/', (requisicao, resposta) => {
     resposta.end(`
